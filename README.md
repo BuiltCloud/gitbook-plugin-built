dcd8cdbca626debc075fc152d823ada44486cec3
kingreatwill <350840291@qq.com>
2020-01-09 15:23:56 +0800

    0.0.2

--- README.md
+++ README.md
@@ -2,12 +2,21 @@ GitBook plugin: built
 ===========================
 ```json
 {
-    "plugins": ["built", "another plugin 1", "another plugin 2"],
+    "plugins": ["built"],
     "pluginsConfig": {
         "logo": {
             "url": "http://www.example.com/my-logo.png",
             "style": "background: none;",
-            "herf": "http://www.example.com",
+            "href": "http://www.example.com",
+        },
+        "favicon": "./img/logo/favicon.ico",
+        "hide-element": {
+            "elements": [".gitbook-link"]
+        },
+        "img-popup": true,
+        "code": {
+            "line": true,
+            "copyButton": true
         }
     }
 }
--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+var fs = require('fs');
+var path = require('path');
+
 module.exports = {
     book: {
         assets: './lib',
@@ -11,7 +14,13 @@ module.exports = {
     // Hook process during build
     hooks: {
         // For all the hooks, this represent the current generator
-
+        "page": function(page) {
+            // img-popup;
+            if(this.options.pluginsConfig && this.options.pluginsConfig["img-popup"]){
+                page.content = page.content + '\n<script>document.onclick = function(e){ e.target.tagName === "IMG" && e.target.id != "book-logo" && window.open(e.target.src,e.target.src)}</script><style>img{cursor:pointer}</style>';
+            }
+            return page;
+        },
         // This is called before the book is generated
         "init": function() {
             console.log("init!");
@@ -19,7 +28,14 @@ module.exports = {
 
         // This is called after the book generation
         "finish": function() {
-            console.log("finish!");
+            // favicon.ico;
+            var pathFile = this.options.pluginsConfig && this.options.pluginsConfig.favicon;
+			var favicon = path.join(process.cwd(), pathFile);
+			var gitbookFaviconPath = path.join(process.cwd(), '_book', 'gitbook', 'images', 'favicon.ico');
+			if (pathFile && fs.existsSync(pathFile) && fs.existsSync(gitbookFaviconPath)){
+				fs.unlinkSync(gitbookFaviconPath);
+				fs.createReadStream(favicon).pipe(fs.createWriteStream(gitbookFaviconPath));
+			}
         }
     }
 
